dc87335e0f68cce9e27806ebc6f31156eddd83e8
Di Truong Ly <ditruongly@gmail.com>
2019-12-14 22:54:39 +0100

    initial

---
+++ Node/12.13.1/vue-hello-client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld :msg="message"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #8bd49b;
+  margin-top: 60px;
+}
+</style>
